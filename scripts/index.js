d870d436b6be14dc1187f1f147b9197fabe1350c
Santoshkumar S <sskumar1999422gmail.com>
2022-01-16 22:26:09 +0530

    Checkpoint 11

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,16 @@ const API_URL = "https://travel-advisor.p.rapidapi.com/";
 const travelAdvisorHost = "travel-advisor.p.rapidapi.com";
 const travelAdvisorKey = "24d38b7b71mshf56ca56c46d27abp13fcfbjsn0b9e0926a234";
 
-
 let viewMore = () => {
     let viewMoreButtonElement = document.getElementById("view-more-button");
     if (viewMoreButtonElement.innerText == "View More") {
         document.getElementById("view-more-city-cards").style.display = "block";
         viewMoreButtonElement.innerText = "View Less";
-    }
-    else {
+    } else {
         document.getElementById("view-more-city-cards").style.display = "none";
         viewMoreButtonElement.innerText = "View More";
     }
-}
-
+};
 
 let debounce = (func, wait, immediate) => {
     let timeout;
@@ -36,81 +33,76 @@ let search = () => {
     let currentFocus;
     let searchInputElement = document.getElementById("search-input");
 
-    searchInputElement.addEventListener("input", debounce(function (e) {
+    searchInputElement.addEventListener(
+        "input",
+        debounce(function (e) {
+            let newValue = this.value;
 
-        let newValue = this.value;
-
-        let xhr = new XMLHttpRequest();
-        if (!newValue || newValue.length < 3) {
-            closeAllLists();
-            return false;
-        }
-
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === this.DONE) {
-
+            let xhr = new XMLHttpRequest();
+            if (!newValue || newValue.length < 3) {
                 closeAllLists();
-                const data = JSON.parse(this.responseText).data;
-                const geos = data.filter((element) => {
-                    return element.result_type === "geos"
-                });
-                let loactionList = [];
-                geos.forEach((element) => {
-                    loactionList.push(element.result_object.name);
-                });
-                currentFocus = -1;
+                return false;
+            }
 
-                let searchListDiv = document.createElement("div");
-                searchListDiv.setAttribute("id", searchInputElement.id + "search-list");
-                searchListDiv.setAttribute("class", "search-items");
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === this.DONE) {
+                    closeAllLists();
+                    const data = JSON.parse(this.responseText).data;
+                    const geos = data.filter((element) => {
+                        return element.result_type === "geos";
+                    });
+                    let loactionList = [];
+                    geos.forEach((element) => {
+                        loactionList.push(element.result_object.name);
+                    });
+                    currentFocus = -1;
 
-                searchInputElement.parentNode.appendChild(searchListDiv);
+                    let searchListDiv = document.createElement("div");
+                    searchListDiv.setAttribute("id", searchInputElement.id + "search-list");
+                    searchListDiv.setAttribute("class", "search-items");
 
-                for (let i = 0; i < loactionList.length; i++) {
+                    searchInputElement.parentNode.appendChild(searchListDiv);
 
-                    if ((loactionList[i].toUpperCase()).includes(newValue.toUpperCase())) {
+                    for (let i = 0; i < loactionList.length; i++) {
+                        if (loactionList[i].toUpperCase().includes(newValue.toUpperCase())) {
+                            let listElementDiv = document.createElement("div");
+                            listElementDiv.setAttribute("onClick", "window.location='list.html?city=" + loactionList[i] + "'");
+                            listElementDiv.innerText = loactionList[i];
 
-                        let listElementDiv = document.createElement("div");
-                        listElementDiv.setAttribute("onClick", "window.location='list.html?city=" + loactionList[i] + "'");
-                        listElementDiv.innerText = loactionList[i];
+                            listElementDiv.innerHTML += "<input type='hidden' value='" + loactionList[i] + "'>";
 
-                        listElementDiv.innerHTML += "<input type='hidden' value='" + loactionList[i] + "'>";
+                            listElementDiv.addEventListener("click", function (e) {
+                                searchInputElement.value = this.getElementsByTagName("input")[0].value;
 
-                        listElementDiv.addEventListener("click", function (e) {
-
-                            searchInputElement.value = this.getElementsByTagName("input")[0].value;
-
-                            closeAllLists();
-                        });
-                        searchListDiv.appendChild(listElementDiv);
+                                closeAllLists();
+                            });
+                            searchListDiv.appendChild(listElementDiv);
+                        }
                     }
                 }
-            }
-        });
+            });
 
-        xhr.open("GET", API_URL + "locations/auto-complete?lang=en_US&units=km&query=" + newValue);
-        xhr.setRequestHeader("x-rapidapi-host", travelAdvisorHost);
-        xhr.setRequestHeader("x-rapidapi-key", travelAdvisorKey);
+            xhr.open("GET", API_URL + "locations/auto-complete?lang=en_US&units=km&query=" + newValue);
+            xhr.setRequestHeader("x-rapidapi-host", travelAdvisorHost);
+            xhr.setRequestHeader("x-rapidapi-key", travelAdvisorKey);
 
-        xhr.send();
+            xhr.send();
+        }, 500)
+    );
 
-    }, 500));
-
-    let closeAllLists = elmnt => {
-
+    let closeAllLists = (elmnt) => {
         let x = document.getElementsByClassName("search-items");
         for (let i = 0; i < x.length; i++) {
             if (elmnt != x[i] && elmnt != searchInputElement) {
                 x[i].parentNode.removeChild(x[i]);
             }
         }
-    }
+    };
 
-    document.addEventListener("click", e => {
+    document.addEventListener("click", (e) => {
         closeAllLists(e.target);
     });
-}
-
+};
 
 search();
 
